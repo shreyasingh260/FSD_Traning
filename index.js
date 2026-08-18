2704f2c686f37263a79a0d7458f0a8312e096e0f
shreyasingh260 <singhshreya29095@gmail.com>
2026-08-18 09:02:13 +0530

    Add login functionality with error handling
    Implement login and loginHandler functions with callback.

---
+++ index.js
@@ -0,0 +1,48 @@
+//console.log("Hello Shruti")
+
+//const sum=function sum(a,b){
+   // return Math.sqrt(a)+Math.sqrt(b);
+//}
+//console.log(sum(16,25));
+
+//const sum=(a,b)=>{return a+b}
+//console.log(sum(3,5))
+
+// (()=>{
+//     console.log("Hey using IIFE")
+// })(); 
+
+// let a=23
+// if(a<40){
+//     let a=40;
+//     console.log("value of a inside block="+a)
+// }
+// console.log("value of a outside block="+a)
+
+//Callback
+// function sum(a,b){
+//     return a+b;
+// }
+// function messwithSum(clbk,msg){
+//     const result=clbk(40,50);
+//     console.log("Hii "+ msg +" and your result="+result)
+// }
+// messwithSum(sum,"Shruti");
+
+function login(error,messg){
+    if(error){
+        console.log("Error is:"+error)
+    }
+    else{
+        console.log(messg)
+    }
+}
+function loginHandler(username,pass,clbk){
+    if(username=="shruti1420" && pass=="12345"){
+        clbk(null,"Login Success")
+    }
+    else{
+        clbk("Username or password is incorrect",null)
+    }
+}
+console.log(loginHandler("shruti1420",1234,login));
